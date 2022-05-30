076f23975c040e6bcee7139fcf1ceb91b37713ef
NebihaAmir <craftsoftwarenebiha@gmail.com>
2022-05-30 22:31:27 +0300

    init commit

---
+++ xyzwebapp/src/main/java/com/xyzwebapp/app/App.java
@@ -0,0 +1,13 @@
+package com.xyzwebapp.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is my first java project using mvn!" );
+    }
+}
